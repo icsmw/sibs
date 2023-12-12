83526f507678dfe157c96132a74d6126cb861368
DmitryAstafyev <dvastafyev@gmail.com>
2023-12-12 09:38:55 +0100

    Support strings in fn arguments

--- src/parser/entry/arguments.rs
+++ src/parser/entry/arguments.rs
@@ -1,13 +1,19 @@
 use crate::parser::{
     chars,
-    entry::{Group, Reader, Reading},
+    entry::{Group, Reader, Reading, ValueString},
     E,
 };
 use uuid::Uuid;
 
 #[derive(Debug, Clone)]
+pub enum Argument {
+    String(String),
+    ValueString(ValueString),
+}
+
+#[derive(Debug, Clone)]
 pub struct Arguments {
-    pub inner: Vec<Vec<(Uuid, String)>>,
+    pub inner: Vec<Vec<(Uuid, Argument)>>,
 }
 
 impl Reading<Arguments> for Arguments {
@@ -31,24 +37,48 @@ impl Arguments {
     pub fn new() -> Self {
         Self { inner: vec![] }
     }
+    pub fn read_string_args(reader: &mut Reader) -> Result<Vec<(Uuid, Argument)>, E> {
+        let mut arguments: Vec<(Uuid, Argument)> = vec![];
+        while let Some((arg, _, uuid)) = reader.read_until_wt(true)? {
+            if !arg.trim().is_empty() {
+                if reader.inherit(arg.clone()).has_char(chars::AT)? {
+                    Err(E::NestedFunction)?
+                }
+                arguments.push((uuid, Argument::String(Reader::unserialize(&arg))));
+            }
+        }
+        if !reader.rest().trim().is_empty() {
+            if reader.has_char(chars::AT)? {
+                Err(E::NestedFunction)?
+            }
+            let (uuid, rest) = reader.to_end();
+            arguments.push((uuid, Argument::String(Reader::unserialize(&rest))));
+        }
+        Ok(arguments)
+    }
     pub fn add_from_group(&mut self, group: Group, parent: &mut Reader) -> Result<(), E> {
         self.add_args(group.inner, parent)?;
         Ok(())
     }
     pub fn add_args(&mut self, inner: String, parent: &mut Reader) -> Result<(), E> {
         let mut reader = parent.inherit(inner);
-        let mut arguments: Vec<(Uuid, String)> = vec![];
-        if reader.has_char(chars::AT)? {
-            Err(E::NestedFunction)?
-        }
-        while let Some((arg, _, uuid)) = reader.read_until_wt(true)? {
-            if !arg.trim().is_empty() {
-                arguments.push((uuid, Reader::unserialize(&arg)));
+        let mut arguments: Vec<(Uuid, Argument)> = vec![];
+        loop {
+            if let Some((before, _, _)) = reader.read_until(&[chars::QUOTES], false, false)? {
+                arguments = [
+                    arguments,
+                    Arguments::read_string_args(&mut reader.inherit(before))?,
+                ]
+                .concat();
+                if let Some(value_string) = ValueString::read(&mut reader)? {
+                    arguments.push((Uuid::new_v4(), Argument::ValueString(value_string)));
+                } else {
+                    Err(E::NoStringEnd)?
+                }
+            } else {
+                arguments = [arguments, Arguments::read_string_args(&mut reader)?].concat();
+                break;
             }
-        }
-        if !reader.rest().trim().is_empty() {
-            let (uuid, rest) = reader.to_end();
-            arguments.push((uuid, Reader::unserialize(&rest)));
         }
         if !arguments.is_empty() {
             self.inner.push(arguments);
