34c1e484f8bac3b97d699e1afc88b18dc2fd6a26
DmitryAstafyev <dvastafyev@gmail.com>
2024-08-12 17:08:48 +0200

    Better error handeling on component

--- cli/src/elements/component.rs
+++ cli/src/elements/component.rs
@@ -66,72 +66,71 @@ impl Reading<Component> for Component {
     fn read(reader: &mut Reader) -> Result<Option<Component>, LinkedErr<E>> {
         let close = reader.open_token(ElTarget::Component);
         let restore = reader.pin();
-        if let Some((before, _)) = reader.until().char(&[&chars::POUND_SIGN]) {
-            if !before.is_empty() {
-                Err(E::UnrecognizedCode(before).by_reader(reader))?;
-            }
-            let _ = reader.move_to().char(&[&chars::POUND_SIGN]);
-            if reader
-                .group()
-                .between(&chars::OPEN_BRACKET, &chars::CLOSE_BRACKET)
-                .is_some()
-            {
-                let mut inner = reader.token()?.bound;
-                let name = inner
-                    .until()
-                    .char(&[&chars::COLON])
-                    .map(|(v, _)| {
-                        inner.move_to().next();
-                        v
-                    })
-                    .unwrap_or_else(|| inner.move_to().end());
-                if name.trim().is_empty() {
-                    Err(E::EmptyComponentName.by_reader(reader))?;
-                }
-                if !Reader::is_ascii_alphabetic_and_alphanumeric(
-                    &name,
-                    &[&chars::UNDERSCORE, &chars::DASH],
-                ) {
-                    Err(E::InvalidComponentName(name.clone()).by_reader(reader))?;
-                }
-                let (name, name_token) = (name, inner.token()?.id);
-                let path = inner.rest().trim().to_string();
-                let inner = if let Some((inner, _)) = reader.until().word(&[words::COMP]) {
-                    inner
-                } else {
-                    reader.move_to().end()
-                };
-                if inner.trim().is_empty() {
-                    Err(E::NoComponentBody.linked(&name_token))?
-                }
-                let mut inner = reader.token()?.bound;
-                let mut elements: Vec<Element> = Vec::new();
-                while let Some(el) =
-                    Element::include(&mut inner, &[ElTarget::Task, ElTarget::Gatekeeper])?
-                {
-                    let _ = inner.move_to().char(&[&chars::SEMICOLON]);
-                    elements.push(el);
-                }
-                Ok(Some(Component {
-                    name: SimpleString {
-                        value: name,
-                        token: name_token,
-                    },
-                    elements,
-                    cwd: if path.is_empty() {
-                        None
-                    } else {
-                        Some(PathBuf::from(path))
-                    },
-                    token: close(reader),
-                }))
-            } else {
-                Err(E::NoComponentDefinition.by_reader(reader))?
-            }
-        } else {
+        let Some((before, _)) = reader.until().char(&[&chars::POUND_SIGN]) else {
             restore(reader);
-            Ok(None)
+            return Ok(None);
+        };
+        if !before.is_empty() {
+            Err(E::UnrecognizedCode(before).by_reader(reader))?;
         }
+        let _ = reader.move_to().char(&[&chars::POUND_SIGN]);
+        if reader
+            .group()
+            .between(&chars::OPEN_BRACKET, &chars::CLOSE_BRACKET)
+            .is_none()
+        {
+            return Err(E::NoComponentDefinition.by_reader(reader));
+        }
+        let mut inner = reader.token()?.bound;
+        let name = inner
+            .until()
+            .char(&[&chars::COLON])
+            .map(|(v, _)| {
+                inner.move_to().next();
+                v
+            })
+            .unwrap_or_else(|| inner.move_to().end());
+        if name.trim().is_empty() {
+            Err(E::EmptyComponentName.by_reader(reader))?;
+        }
+        if !Reader::is_ascii_alphabetic_and_alphanumeric(&name, &[&chars::UNDERSCORE, &chars::DASH])
+        {
+            Err(E::InvalidComponentName(name.clone()).by_reader(reader))?;
+        }
+        let (name, name_token) = (name, inner.token()?.id);
+        let path = inner.rest().trim().to_string();
+        let rest = if let Some((rest, _)) = reader.until().word(&[words::COMP]) {
+            rest
+        } else {
+            reader.move_to().end()
+        };
+        if rest.trim().is_empty() {
+            Err(E::NoComponentBody.linked(&name_token))?
+        }
+        let mut inner = reader.token()?.bound;
+        let inner_token_id = reader.token()?.id;
+        let mut elements: Vec<Element> = Vec::new();
+        while let Some(el) = Element::include(&mut inner, &[ElTarget::Task, ElTarget::Gatekeeper])?
+        {
+            let _ = inner.move_to().char(&[&chars::SEMICOLON]);
+            elements.push(el);
+        }
+        if elements.is_empty() {
+            return Err(E::UnrecognizedCode(rest).linked(&inner_token_id));
+        }
+        Ok(Some(Component {
+            name: SimpleString {
+                value: name,
+                token: name_token,
+            },
+            elements,
+            cwd: if path.is_empty() {
+                None
+            } else {
+                Some(PathBuf::from(path))
+            },
+            token: close(reader),
+        }))
     }
 }
 
@@ -340,7 +339,7 @@ mod reading {
         let samples = include_str!("../tests/error/component.sibs");
         let samples = samples
             .split('\n')
-            .map(|v| format!("{v} {{\nenv::is_os();\n}};"))
+            .map(|v| format!("{v} task {{\nenv::is_os();\n}};"))
             .collect::<Vec<String>>();
         let mut count = 0;
         for sample in samples.iter() {
