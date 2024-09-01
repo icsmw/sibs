7ac1db6683385cb78ca4c01bef0a9a2cecd1f772
DmitryAstafyev <dvastafyev@gmail.com>
2024-09-02 00:06:45 +0200

    Basic proptest for ppm

--- cli/src/elements/ppm.rs
+++ cli/src/elements/ppm.rs
@@ -214,3 +214,79 @@ mod reading {
         );
     }
 }
+
+#[cfg(test)]
+mod proptest {
+
+    use crate::{
+        elements::{ElTarget, Element, Ppm, PpmCall, Task},
+        error::LinkedErr,
+        inf::{operator::E, tests::*, Configuration},
+        read_string,
+        reader::{Dissect, Reader, Sources},
+    };
+    use proptest::prelude::*;
+
+    impl Arbitrary for PpmCall {
+        type Parameters = usize;
+        type Strategy = BoxedStrategy<Self>;
+        fn arbitrary_with(deep: Self::Parameters) -> Self::Strategy {
+            Element::arbitrary_with((
+                vec![
+                    ElTarget::Function,
+                    ElTarget::VariableName,
+                    ElTarget::Integer,
+                ],
+                deep,
+            ))
+            .prop_map(|el| {
+                if matches!(el, Element::Function(..)) {
+                    PpmCall::Function(Box::new(el))
+                } else {
+                    PpmCall::VectorElementAccessor(Box::new(el))
+                }
+            })
+            .boxed()
+        }
+    }
+    impl Arbitrary for Ppm {
+        type Parameters = usize;
+        type Strategy = BoxedStrategy<Self>;
+
+        fn arbitrary_with(deep: Self::Parameters) -> Self::Strategy {
+            PpmCall::arbitrary_with(deep)
+                .prop_map(|call| Ppm { token: 0, call })
+                .boxed()
+        }
+    }
+
+    fn reading(ppm: Ppm) {
+        get_rt().block_on(async {
+            let origin = format!("@test {{\nsome_initial_func(){ppm};\n}};");
+            read_string!(
+                &Configuration::logs(false),
+                &origin,
+                |reader: &mut Reader, src: &mut Sources| {
+                    let task = src
+                        .report_err_if(Task::dissect(reader))?
+                        .expect("Task read");
+                    assert_eq!(format!("{task};"), origin);
+                    Ok::<(), LinkedErr<E>>(())
+                }
+            );
+        })
+    }
+
+    proptest! {
+        #![proptest_config(ProptestConfig {
+            max_shrink_iters: 5000,
+            ..ProptestConfig::with_cases(10)
+        })]
+        #[test]
+        fn test_run_task(
+            args in any_with::<Ppm>(2)
+        ) {
+            reading(args.clone());
+        }
+    }
+}
