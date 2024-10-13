c082ce045e49a15fde737ee0b5e92e9f003e0aa2
DmitryAstafyev <dvastafyev@gmail.com>
2024-10-13 22:03:37 +0200

    Updateprocessing tests for elements (if, optional)

--- cli/src/elements/optional/tests.rs
+++ cli/src/elements/optional/tests.rs
@@ -1,6 +1,6 @@
 use crate::{
     elements::{Element, ElementRef, InnersGetter, Optional},
-    test_reading_el_by_el, test_reading_with_errors_ln_by_ln,
+    test_process_tasks_one_by_one, test_reading_el_by_el, test_reading_with_errors_ln_by_ln,
 };
 
 impl InnersGetter for Optional {
@@ -23,46 +23,9 @@ test_reading_with_errors_ln_by_ln!(
     7
 );
 
-#[cfg(test)]
-mod processing {
-    use crate::{
-        elements::{Element, ElementRef},
-        error::LinkedErr,
-        inf::{
-            operator::{Execute, E},
-            Configuration, Context, ExecuteContext, Journal, Scope,
-        },
-        process_string,
-        reader::{chars, Reader, Sources},
-    };
-
-    #[tokio::test]
-    async fn processing() {
-        process_string!(
-            &Configuration::logs(false),
-            &include_str!("../../tests/processing/optional.sibs"),
-            |reader: &mut Reader, src: &mut Sources| {
-                let mut tasks: Vec<Element> = Vec::new();
-                while let Some(task) =
-                    src.report_err_if(Element::include(reader, &[ElementRef::Task]))?
-                {
-                    let _ = reader.move_to().char(&[&chars::SEMICOLON]);
-                    tasks.push(task);
-                }
-                Ok::<Vec<Element>, LinkedErr<E>>(tasks)
-            },
-            |tasks: Vec<Element>, cx: Context, sc: Scope, _: Journal| async move {
-                for task in tasks.iter() {
-                    let result = task
-                        .execute(ExecuteContext::unbound(cx.clone(), sc.clone()))
-                        .await?;
-                    assert_eq!(
-                        result.as_string().expect("Task returns string value"),
-                        "true".to_owned()
-                    );
-                }
-                Ok::<(), LinkedErr<E>>(())
-            }
-        );
-    }
-}
+test_process_tasks_one_by_one!(
+    processing,
+    &include_str!("../../tests/processing/optional.sibs"),
+    true,
+    3
+);
--- cli/src/elements/statements/if/tests.rs
+++ cli/src/elements/statements/if/tests.rs
@@ -1,6 +1,6 @@
 use crate::{
     elements::{Element, ElementRef, If, InnersGetter},
-    test_reading_el_by_el, test_reading_with_errors_ln_by_ln,
+    test_process_tasks_one_by_one, test_reading_el_by_el, test_reading_with_errors_ln_by_ln,
 };
 
 impl InnersGetter for If {
@@ -26,52 +26,9 @@ test_reading_with_errors_ln_by_ln!(
     15
 );
 
-#[cfg(test)]
-mod processing {
-    use crate::{
-        elements::{Element, ElementRef},
-        error::LinkedErr,
-        inf::{
-            operator::{Execute, ExecuteContext, E},
-            Configuration, Context, Journal, Scope,
-        },
-        process_string,
-        reader::{chars, Reader, Sources},
-    };
-
-    #[tokio::test]
-    async fn processing() {
-        let tasks_count = include_str!("../../../tests/processing/if.sibs")
-            .match_indices(chars::AT)
-            .count();
-        process_string!(
-            &Configuration::logs(false),
-            &include_str!("../../../tests/processing/if.sibs"),
-            |reader: &mut Reader, src: &mut Sources| {
-                let mut tasks: Vec<Element> = Vec::new();
-                while let Some(task) =
-                    src.report_err_if(Element::include(reader, &[ElementRef::Task]))?
-                {
-                    let _ = reader.move_to().char(&[&chars::SEMICOLON]);
-                    tasks.push(task);
-                }
-                Ok::<Vec<Element>, LinkedErr<E>>(tasks)
-            },
-            |tasks: Vec<Element>, cx: Context, sc: Scope, _: Journal| async move {
-                for (i, task) in tasks.iter().enumerate() {
-                    let result = task
-                        .execute(ExecuteContext::unbound(cx.clone(), sc.clone()))
-                        .await?;
-                    assert_eq!(
-                        result.as_string().expect("if returns string value"),
-                        "true".to_owned(),
-                        "Line: {}",
-                        i + 1
-                    );
-                }
-                assert_eq!(tasks_count, tasks.len());
-                Ok::<(), LinkedErr<E>>(())
-            }
-        );
-    }
-}
+test_process_tasks_one_by_one!(
+    processing,
+    &include_str!("../../../tests/processing/if.sibs"),
+    true,
+    99
+);
