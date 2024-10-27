a1f3c9f6d867eef4ccd68c3aabb483707efd2f54
DmitryAstafyev <dvastafyev@gmail.com>
2024-10-27 02:39:19 +0200

    Set more proptest usecases

--- interpreter/src/lexer/tests/proptest.rs
+++ interpreter/src/lexer/tests/proptest.rs
@@ -5,7 +5,7 @@ use proptest::prelude::*;
 proptest! {
     #![proptest_config(ProptestConfig {
         max_shrink_iters: 50,
-        ..ProptestConfig::with_cases(100)
+        ..ProptestConfig::with_cases(500)
     })]
 
     #[test]
