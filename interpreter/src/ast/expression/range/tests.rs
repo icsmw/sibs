b10763d29c45e0cbb168728f644f1772d0997647
DmitryAstafyev <dvastafyev@gmail.com>
2025-04-16 19:34:07 +0200

    Add tests

--- interpreter/src/ast/expression/range/tests.rs
+++ interpreter/src/ast/expression/range/tests.rs
@@ -8,3 +8,17 @@ test_value_expectation!(
     RtValue::Range(RangeInclusive::new(10, 20)),
     "{ let a = 10..20; a; }"
 );
+
+
+test_value_expectation!(
+    range_001,
+    Block,
+    RtValue::Num(55.0),
+    r#"{
+        let sum = 0;
+        for(el, n) in 0..10 {
+            sum = sum + n;
+        };
+        sum;
+    }"#
+);
--- interpreter/src/ast/statement/while/tests.rs
+++ interpreter/src/ast/statement/while/tests.rs
@@ -41,3 +41,39 @@ test_value_expectation!(
         n;
     }"#
 );
+
+test_value_expectation!(
+    r#while_003,
+    Block,
+    RtValue::Num(20.0),
+    r#"{
+        let n = 0;
+        let m = 0;
+        while (n < 20 && m < 20) {
+            n += 1;
+            m += 1;
+            if n == 10 {
+                break;
+            };
+        };
+        n + m;
+    }"#
+);
+
+test_value_expectation!(
+    r#while_004,
+    Block,
+    RtValue::Num(20.0),
+    r#"{
+        let n = 0;
+        let m = 0;
+        while n < 20 && m < 20 {
+            n += 1;
+            m += 1;
+            if n == 10 {
+                break;
+            };
+        };
+        n + m;
+    }"#
+);
